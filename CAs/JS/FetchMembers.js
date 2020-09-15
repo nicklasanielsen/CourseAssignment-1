dffa7b8bd251e23f30740a96acbd8358e8dfefa3
Nikolaj <cph-nl174@cphbusiness.dk>
2020-09-15 16:30:15 +0200

    JS fetch frontend

--- CAs/JS/FetchMembers.js
+++ CAs/JS/FetchMembers.js
@@ -6,21 +6,17 @@ btn.addEventListener('click', function(e) {
 });
 
 function fetchAllMembers() {
-    let url = 'https://www.newbiz.nicklasnielsen.dk/api/groupmembers/all';
+    let url = 'http://localhost:8080/CourseAssignment-1/api/groupmembers/all';
     let allMembers = document.getElementById("table_body");
-
-    let count = 1;
-
+    
     fetch(url)
             .then(res => res.json())
             .then(data => {
                 let membersArray = data.map(member => `<tr>
-                <td>${count}</td>
-                <td>${member.name}</td>
+                <td>${member.fullname}</td>
                 <td>${member.studentID}</td>
                 <td>${member.github}</td>
-                </tr>`,
-                count++);
+                </tr>`);
 
                 allMembers.innerHTML = membersArray.join("");
 
