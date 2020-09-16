61a184b20decfb382427dc5dbc00e34fcae15162
Haugaard <mathiashn9@gmail.com>
2020-09-16 12:18:19 +0200

    all function and syntax changes

--- CAs/JS/FetchJokes.js
+++ CAs/JS/FetchJokes.js
@@ -1,20 +1,20 @@
-let btn = document.getElementById("Fetch_Joke");
+let btn = document.getElementById("fetch_Joke");
 
-btn.addEventListener("submit", function(e){
+btn.addEventListener('submit', function(e){
     e.preventDefault();
     let jokeByID = document.getElementById("jokeID");
     fetchJokeByID(jokeByID.value);
 });
 
-let randomBtn = document.getElementById("RandomJokeBtn");
+let randomBtn = document.getElementById("randomJokeBtn");
 
-randomBtn.addEventListener("click",  function(e){
+randomBtn.addEventListener('click',  function(e){
     e.preventDefault();
     fetchRandomJoke();
 });
 
 function fetchJokeByID(id){
-    let url = 'http://newbiz.mathaugaard.dk/CourseAssignment-1/api/jokes/id/' + id;
+    let url = 'http://newbiz.nicklasnielsen.dk/CourseAssignment-1/api/jokes/id/' + id;
     let selectedJoke = document.getElementById("table_body");
     
     fetch(url)
@@ -33,7 +33,7 @@ function fetchJokeByID(id){
 }
 
 function fetchRandomJoke(){
-    let url = 'http://newbiz.mathaugaard.dk/CourseAssignment-1/api/jokes/random';
+    let url = 'http://newbiz.nicklasnielsen.dk/CourseAssignment-1/api/jokes/random';
     let selectedJoke = document.getElementById("table_body");
     
     fetch(url)
@@ -48,4 +48,29 @@ function fetchRandomJoke(){
                     selectedJoke.innerHTML = sj.join("");
                 
                 });
+}
+
+let btn = document.getElementById("getAllJokesBtn");
+
+btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    fetchAllJokes();
+});
+
+function fetchAllJokes() {
+    let url = 'http://newbiz.nicklasnielsen.dk/CourseAssignment-1/api/jokes/all';
+    let allJokes = document.getElementById("table_body");
+    
+    fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                let jokeArray = data.map(j => `<tr>
+                <td>${j.joke}</td>
+                <td>${j.reference}</td>
+                <td>${j.type}</td>
+                </tr>`);
+
+                allJokes.innerHTML = jokeArray.join("");
+
+            });
 }
