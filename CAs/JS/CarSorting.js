c2f8aa8edc179a51128bc421f02f88446f4fa40b
Haugaard <mathiashn9@gmail.com>
2020-09-18 11:12:26 +0200

    Car sorting fixes

--- CAs/JS/CarSorting.js
+++ CAs/JS/CarSorting.js
@@ -28,93 +28,119 @@ sort4.addEventListener('click', function (e) {
 });
 
 function sortByYear() {
-    let cars = fetchAllCars();
+    let url = './api/cars/all';
     let table = document.getElementById("table_body");
 
-    let sortedByYear = cars.sort(function (a, b) {
-        if (a.year < b.year) {
-            return -1;
-        }
-        if (a.year > b.year) {
-            return 1;
-        }
-        return 0;
-    });
+    fetch(url)
+            .then(res => res.json())
+            .then(data => {
 
-    let tableArray = sortedByYear.map(car => `<tr>
+
+                let sortedByYear = data.sort(function (a, b) {
+                    if (a.year < b.year) {
+                        return -1;
+                    }
+                    if (a.year > b.year) {
+                        return 1;
+                    }
+                    return 0;
+                });
+
+                let tableArray = sortedByYear.map(car => `<tr>
             <td>${car.id}</td>
             <td>${car.year}</td>
             <td>${car.make}</td>
             <td>${car.model}</td>
             <td>${car.price}</td>
             </tr>`
-    )
+                )
 
-    table.innerHTML = tableArray.join("");
+                table.innerHTML = tableArray.join("");
+            });
+
 }
 
 function sortByModel() {
-    let cars = fetchAllCars();
+    let url = './api/cars/all';
     let table = document.getElementById("table_body");
 
-    let sortedByModel = cars.sort(function (a, b) {
-        return a.model.toLowerCase().localeCompare(b.model.toLowerCase());
-    });
-    
-    let tableArray = sortedByYear.map(car => `<tr>
+    fetch(url)
+            .then(res => res.json())
+            .then(data => {
+
+                let sortedByModel = data.sort(function (a, b) {
+                    return a.model.toLowerCase().localeCompare(b.model.toLowerCase());
+                });
+
+                let tableArray = sortedByModel.map(car => `<tr>
             <td>${car.id}</td>
             <td>${car.year}</td>
             <td>${car.make}</td>
             <td>${car.model}</td>
             <td>${car.price}</td>
             </tr>`
-    )
+                )
 
-    table.innerHTML = tableArray.join("");
+                table.innerHTML = tableArray.join("");
+            });
+
 }
 
 function sortByMake() {
-    let cars = fetchAllCars();
+    let url = './api/cars/all';
     let table = document.getElementById("table_body");
 
-    let sortedByMake = cars.sort(function (a, b) {
-        return a.make.toLowerCase().localeCompare(b.make.toLowerCase());
-    });
-    
-    let tableArray = sortedByYear.map(car => `<tr>
+    fetch(url)
+            .then(res => res.json())
+            .then(data => {
+
+                let sortedByMake = data.sort(function (a, b) {
+                    return a.make.toLowerCase().localeCompare(b.make.toLowerCase());
+                });
+
+                let tableArray = sortedByMake.map(car => `<tr>
             <td>${car.id}</td>
             <td>${car.year}</td>
             <td>${car.make}</td>
             <td>${car.model}</td>
             <td>${car.price}</td>
             </tr>`
-    )
+                )
 
-    table.innerHTML = tableArray.join("");
+                table.innerHTML = tableArray.join("");
+            });
+
+
 }
 
 function sortByPrice() {
-    let cars = fetchAllCars();
+    let url = './api/cars/all';
     let table = document.getElementById("table_body");
 
-    let sortedByPrice = cars.sort(function (a, b) {
-        if (a.price < b.price) {
-            return -1;
-        }
-        if (a.price > b.price) {
-            return 1;
-        }
-        return 0;
-    });
-    
-    let tableArray = sortedByYear.map(car => `<tr>
+    fetch(url)
+            .then(res => res.json())
+            .then(data => {
+
+                let sortedByPrice = data.sort(function (a, b) {
+                    if (a.price < b.price) {
+                        return -1;
+                    }
+                    if (a.price > b.price) {
+                        return 1;
+                    }
+                    return 0;
+                });
+
+                let tableArray = sortedByPrice.map(car => `<tr>
             <td>${car.id}</td>
             <td>${car.year}</td>
             <td>${car.make}</td>
             <td>${car.model}</td>
             <td>${car.price}</td>
             </tr>`
-    )
+                )
 
-    table.innerHTML = tableArray.join("");
+                table.innerHTML = tableArray.join("");
+            });
+
 }
