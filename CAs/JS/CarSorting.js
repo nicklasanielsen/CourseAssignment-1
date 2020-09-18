f07b4655863503c82e82ef9ee5a8276a53a4c219
Haugaard <mathiashn9@gmail.com>
2020-09-18 09:43:34 +0200

    Car sort

---
+++ CAs/JS/CarSorting.js
@@ -0,0 +1,80 @@
+let sort1 = document.getElementById("sortByYear");
+let sort2 = document.getElementById("sortByModel");
+let sort3 = document.getElementById("sortByMake");
+let sort4 = document.getElementById("sortByPrice");
+
+sort1.addEventListener('click', function(e){
+   e.preventDefault();
+   sortByYear();
+});
+
+sort2.addEventListener('click', function(e){
+   e.preventDefault();
+   sortByModel();
+});
+
+sort3.addEventListener('click', function(e){
+   e.preventDefault();
+   sortByMake();
+});
+
+sort4.addEventListener('click', function(e){
+   e.preventDefault();
+   sortByPrice();
+});
+
+function sortByYear(){
+    let cars = fetchAllCars();
+    let table = document.getElementById("table_body");
+    
+    let sortedByYear = cars.sort(function(a, b){
+        if(a.year < b.year){
+            return -1;
+        }
+        if(a.year > b.year){
+            return 1;
+        }
+        return 0;
+    });
+    
+    table.innerHTML = sortedByYear.join("");    
+}
+
+function sortByModel(){
+    let cars = fetchAllCars();
+    let table = document.getElementById("table_body");
+    
+    let sortedByModel = cars.sort(function(a, b){
+        return a.model.toLowerCase().localeCompare(b.model.toLowerCase());
+    });
+    
+    table.innerHTML = sortedByModel.join("");
+}
+
+function sortByMake(){
+    let cars = fetchAllCars();
+    let table = document.getElementById("table_body");
+    
+    let sortedByMake = cars.sort(function(a, b){
+        return a.make.toLowerCase().localeCompare(b.make.toLowerCase());
+    });
+    
+    table.innerHTML = sortedByMake.join("");
+}
+
+function sortByPrice(){
+    let cars = fetchAllCars();
+    let table = document.getElementById("table_body");
+    
+    let sortedByPrice = cars.sort(function(a, b){
+        if(a.price < b.price){
+            return -1;
+        }
+        if(a.price > b.price){
+            return 1;
+        }
+        return 0;
+    });
+    
+    table.innerHTML = sortedByPrice.join("");
+}
